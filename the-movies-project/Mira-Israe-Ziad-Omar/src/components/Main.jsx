951d54f829cf41135ea6c88099ac40b382c19eb7
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-30 02:48:49 +0300

    Version 2

--- the-movies-project/Mira-Israe-Ziad-Omar/src/components/Main.jsx
+++ the-movies-project/Mira-Israe-Ziad-Omar/src/components/Main.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
-const Main = () => {
-  return <div></div>;
+import React, { useState } from "react";
+import { Spinner } from "react-bootstrap";
+const Main = ({ showSpinner }) => {
+  return (
+    <div style= {{marginBottom : "12%"}}>
+      {showSpinner && (
+        <Spinner
+          style={{ textAlign: "center", marginTop: "25%", marginLeft: "50%" }}
+          id="spinner"
+          showSpinner={showSpinner}
+          animation="grow"
+          variant="info"
+        />
+      )}
+    </div>
+  );
 };
-
 export default Main;
--- the-movies-project/Mira-Israe-Ziad-Omar/src/components/Navi.jsx
+++ the-movies-project/Mira-Israe-Ziad-Omar/src/components/Navi.jsx
@@ -1,18 +1,25 @@
-import { Navbar, Nav, Container} from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import React from "react";
 import SearchBar from "./Searchbar";
 
-const Navi = () => {
+const Navi = ({ setShowSpinner, handleQuery }) => {
   return (
-    <Navbar bg="dark" className= "px-0">
-      <Container fluid className= "mx-0 px-0">
-        <Navbar.Brand className = " px-3 mx-0 text-white font-weight-bolder" href="#">Movies</Navbar.Brand>
+    <Navbar bg="dark" className="px-0">
+      <Container fluid className="mx-0 px-0">
+        <Navbar.Brand
+          className=" px-3 mx-0 text-white font-weight-bolder"
+          href="#"
+        >
+          Movies
+        </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link className="text-white font-size-4" href="#">Home</Nav.Link>
+          <Nav.Link className="text-white font-size-4" href="#">
+            Home
+          </Nav.Link>
         </Nav>
-        <SearchBar />
+        <SearchBar setShowSpinner={setShowSpinner} handleQuery={handleQuery} />
       </Container>
-    </Navbar>  
+    </Navbar>
   );
 };
 
