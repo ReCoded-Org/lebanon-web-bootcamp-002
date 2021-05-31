40aaaa4bcc5f4d04f077fad3350a32782374621b
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-05-31 12:04:22 +0300

    V2

---
+++ the-movies-project/Charbel-Mohamad/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
